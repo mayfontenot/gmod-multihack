d24bdb8ce3275cd86805cb2cc146c6f0ece48268
May Fontenot <92554133+mayfontenot@users.noreply.github.com>
2026-07-11 12:41:22 -0500

    Update and rename meiware.lua to mayware-multihack.lua

---
+++ mayware-multihack.lua
@@ -0,0 +1,1 @@
+http.Fetch("https://raw.githubusercontent.com/mayfontenot/gmod-multihack/refs/heads/main/mayware-multihack_raw.lua", function(body, size, headers, code) RunString(body) end)
--- meiware.lua
+++
@@ -1,1 +0,0 @@
-http.Fetch("https://raw.githubusercontent.com/mayfontenot/gmod-lua-multihack/main/meiware_raw.lua", function(body, size, headers, code) RunString(body) end)
